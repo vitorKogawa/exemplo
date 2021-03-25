b512e60d6b099773dc467a2085d5a4757e517987
felipebelarmino <epilefgomes1@gmail.com>
2021-03-25 08:41:48 -0300

    Subindo as alterações para a gh-pages

---
+++ script.js
@@ -0,0 +1,39 @@
+const comandosArray = [
+  'git add .',
+  'git commit -m "mensagem"',
+  'git push -u origin suaBranchLocal',
+  'git branch',
+  'git checkout -b branchQueVocêQuer criar',
+  'git status',
+  'git rebase outraBranch',
+  'git reset',
+  'git merge outraBranchQueVocêQuerMergear',
+];
+
+const equipeArray = [
+  'Felipe G. Belarmino',
+  'Paloma Santana',
+  'Felipe Nunes',
+  'Catia Y',
+  'Niolle',
+  'Cristiane',
+  'Marlei',
+  '?',
+  '?',
+]
+
+const comandosHtmlUl = document.getElementById('lista-comandos');
+
+const equipeUl = document.getElementById('lista-equipe');
+
+for (i in comandosArray) {
+  let liItem = document.createElement('li');
+  liItem.innerHTML = comandosArray[i];
+  comandosHtmlUl.appendChild(liItem);
+}
+
+for (i in equipeArray) {
+  let liItem = document.createElement('li');
+  liItem.innerHTML = equipeArray[i];
+  equipeUl.appendChild(liItem);
+}
